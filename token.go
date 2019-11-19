8adb3c2971578a04520037e8eb0b1abc3e46648e
danfragoso <danilo.fragoso@gmail.com>
2019-11-19 12:26:49 -0300

    Tokenizer passed it`s first test 🎉

--- token.go
+++ token.go
@@ -1,12 +1,54 @@
 package sausage
 
+import (
+	"fmt"
+	"strings"
+)
+
 //Tokenize - Reads source code and tokenizes it, return can be slice with tokens or an error
 func Tokenize(source string) interface{} {
 	var tokens []*Token
+	sourceOriginal := source
 
 	if len(source) == 0 {
 		return tokens
 	}
 
-	return nil
+	for len(source) > 0 {
+		var currentToken string
+
+		if currentToken == "" {
+			for tokenType, tokenPattern := range tokenPatterns {
+				tokenIndex := tokenPattern.FindStringIndex(source)
+
+				if len(tokenIndex) > 0 {
+					if tokenIndex[0] == 0 {
+						rangeDiff := len(sourceOriginal) - len(source)
+
+						var absoluteRange []int
+
+						currentToken = source[tokenIndex[0]:tokenIndex[1]]
+
+						absoluteRange = append(absoluteRange, tokenIndex[0]+rangeDiff)
+						absoluteRange = append(absoluteRange, tokenIndex[1]+rangeDiff)
+
+						tokens = append(tokens, &Token{
+							Type:  tokenType,
+							Value: currentToken,
+							Range: absoluteRange,
+						})
+
+						source = strings.TrimSpace(source[tokenIndex[1]:len(source)])
+
+						currentToken = ""
+					}
+				}
+			}
+		}
+	}
+
+	for _, v := range tokens {
+		fmt.Println(v)
+	}
+	return tokens
 }
--- token_type.go
+++ token_type.go
@@ -1,10 +1,12 @@
 package sausage
 
+import "regexp"
+
 /*Token - Token format*/
 type Token struct {
-	Type  string   `json:"type"`
-	Value string   `json:"value"`
-	Range [2]int32 `json:"range"`
+	Type  string `json:"type"`
+	Value string `json:"value"`
+	Range []int  `json:"range"`
 }
 
 /*TokenizerFailure - Tokenizer failure format*/
@@ -14,4 +16,16 @@ type TokenizerFailure struct {
 	LineNumber int32  `json:"lineNumber"`
 	Column     int32  `json:"column"`
 	Message    string `json:"message"`
+}
+
+var tokenPatterns = map[string]*regexp.Regexp{
+	"Keyword":           regexp.MustCompile(`^abstract|arguments|await|boolean|break|byte|case|catch|char|class|const|continue|debugger|default|delete|do|double|else|enum|eval|export|extends|false|final|finally|float|for|function|goto|if|implements|import|in|instanceof|int|interface|let|long|native|new|null|package|private|protected|public|return|short|static|super|switch|synchronized|this|throw|throws|transient|true|try|typeof|var|void|volatile|while|with|yield`),
+	"NullLiteral":       regexp.MustCompile(`^null`),
+	"BooleanLiteral":    regexp.MustCompile(`^true|false`),
+	"NumericLiteral":    regexp.MustCompile(`^\d+`),
+	"Punctuator":        regexp.MustCompile(`^{|\(|\)|\[|\]|\.{3}|\.|;|,|<<=|>>>=|>>=|>{3}|>{2}|<{2}|<=|>=|=>|<|>|===|!==|==|!=|\+=|\+{2}|\+|\-=|-{2}|\-|\*{2}=|\*{2}|\*=|\*|%=|%|&{2}|&=|&|\|{2}|\|=|\||~|\?|:|\^=|\^|!|/=|=|}`),
+	"StringLiteral":     regexp.MustCompile(`^["|'].*["|']`),
+	"RegularExpression": regexp.MustCompile(`\/.*\/y?m?g?i?u?s?`),
+	"Identifier":        regexp.MustCompile(`^[a-zA-z|$]+`),
+	"Template":          regexp.MustCompile(`^\x60.*\x60`),
 }
