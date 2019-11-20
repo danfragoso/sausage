ba08d2d9ec577fbc0219b45612dfcd254e388907
danfragoso <danilo.fragoso@gmail.com>
2019-11-20 16:26:55 -0300

    tokenizer cleanup

--- token.go
+++ token.go
@@ -17,30 +17,22 @@ func Tokenize(source string) interface{} {
 		var currentToken string
 
 		if currentToken == "" {
-			source = strings.Trim(source, " \n\r")
+			source = removeImediateWhitespace(source)
 
 			for tokenType, tokenPattern := range tokenPatterns {
 				tokenIndex := tokenPattern.FindStringIndex(source)
 
 				if len(tokenIndex) > 0 {
 					if tokenIndex[0] == 0 {
-						rangeDiff := len(sourceOriginal) - len(source)
-
-						var absoluteRange []int
-
-						currentToken = source[tokenIndex[0]:tokenIndex[1]]
-
-						absoluteRange = append(absoluteRange, tokenIndex[0]+rangeDiff)
-						absoluteRange = append(absoluteRange, tokenIndex[1]+rangeDiff)
+						currentToken = getCurrentToken(source, tokenIndex)
 
 						tokens = append(tokens, &Token{
 							Type:  tokenType,
 							Value: currentToken,
-							Range: absoluteRange,
+							Range: getAbsoluteRange(sourceOriginal, source, tokenIndex),
 						})
 
-						source = strings.TrimSpace(source[tokenIndex[1]:len(source)])
-
+						source = removeCurrentTokenFromSource(source, tokenIndex)
 						currentToken = ""
 					}
 				}
@@ -49,4 +41,26 @@ func Tokenize(source string) interface{} {
 	}
 
 	return tokens
+}
+
+func removeImediateWhitespace(source string) string {
+	return strings.Trim(source, " \n\r")
+}
+
+func getAbsoluteRange(originalString string, sourceString string, tokenIndex []int) []int {
+	rangeDiff := len(originalString) - len(sourceString)
+
+	var absoluteRange []int
+	absoluteRange = append(absoluteRange, tokenIndex[0]+rangeDiff)
+	absoluteRange = append(absoluteRange, tokenIndex[1]+rangeDiff)
+
+	return absoluteRange
+}
+
+func getCurrentToken(source string, tokenIndex []int) string {
+	return source[tokenIndex[0]:tokenIndex[1]]
+}
+
+func removeCurrentTokenFromSource(source string, tokenIndex []int) string {
+	return strings.TrimSpace(source[tokenIndex[1]:len(source)])
 }
